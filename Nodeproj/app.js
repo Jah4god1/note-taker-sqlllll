576cb3e4cdfa02fe6c1779c0fa185b445300f5f6
Japheth <japhethbruce93@gmail.com>
2023-07-17 09:06:15 -0500

    route for html

--- Nodeproj/app.js
+++ Nodeproj/app.js
@@ -7,3 +7,25 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Route for serving the index.html file
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+    });
+    
+    app.post('/notes', (req, res) => {
+    const { title, description } = req.body;
+    const sql = 'INSERT INTO notes (title, description) VALUES (?, ?)';
+    connection.query(sql, [title, description], (error, results) => {
+    if (error) {
+    console.error('Error inserting note:', error);
+    res.status(500).json({ error: 'Failed to insert note' });
+    return;
+    }
+    res.json({ message: 'Note created successfully' });
+    });
+    });
+    
+    app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+    });
