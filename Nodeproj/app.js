0dcf9d9c31f363cd6fb0cf710196c72413e922cc
Japheth <japhethbruce93@gmail.com>
2023-07-18 08:17:53 -0500

    functioning

--- Nodeproj/app.js
+++ Nodeproj/app.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const mysql = require('mysql2');
 const connection = require('./connection');
+const path = require('path');
 
 const app = express();
-const port = 5500;
+const port = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Route for serving the index.html file
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + './index.html');
+    app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
     });
-    
+   
     app.post('/notes', (req, res) => {
     const { title, description } = req.body;
     const sql = 'INSERT INTO notes (title, description) VALUES (?, ?)';
+    
     connection.query(sql, [title, description], (error, results) => {
     if (error) {
     console.error('Error inserting note:', error);
--- Nodeproj/connection.js
+++ Nodeproj/connection.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
 
-const app = express();
-const port = 5500;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 // Create a MySQL connection
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -23,26 +17,9 @@ connection.connect((err) => {
   console.log('Connected to the database note_taker.');
 });
 
-// Serve the index.html file
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + './index.html');
-});
+module.exports = connection;
 
-// Handle the POST request to create a note
-app.post('/notes', (req, res) => {
-  const { title, description } = req.body;
-  const sql = 'INSERT INTO notes (title, description) VALUES (?, ?)';
-  connection.query(sql, [title, description], (error, results) => {
-    if (error) {
-      console.error('Error inserting note:', error);
-      res.status(500).json({ error: 'Failed to insert note' });
-      return;
-    }
-    res.json({ message: 'Note created successfully' });
-  });
-});
+// Serve the index.html file
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+
